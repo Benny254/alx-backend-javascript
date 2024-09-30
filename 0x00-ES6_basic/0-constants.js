6178bb8965f35cff667d94eea2db2c91fb4fa5db
Benny254 <asshimbenny01@gmail.com>
2024-09-30 12:43:59 +0000

    update

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,15 +1,9 @@
-export function taskFirst() {
+function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
 }
 
-export function getLast() {
-  return ' is okay';
-}
-
-export function taskNext() {
-  let combination = 'But sometimes let';
-  combination += getLast();
-
-  return combination;
+function taskNext() {
+  let task = 'But sometimes let is okay.';
+  return task;
 }
