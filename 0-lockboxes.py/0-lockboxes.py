8f7727470d038dbb8a78d0f25c300282c8b7773b
Benny254 <asshimbenny01@gmail.com>
2024-10-09 20:53:19 +0000

    update

--- 0-lockboxes.py/0-lockboxes.py
+++ 0-lockboxes.py/0-lockboxes.py
@@ -1,15 +1,32 @@
+#!/usr/bin/python3
+'''LockBoxes Challenge'''
+
+
 def canUnlockAll(boxes):
-    n = len(boxes)
-    opened = [False] * n
-    opened[0] = True
-    keys = [0]
+    '''determines if all the boxes can be opened or not
+    Returns:
+        True: all boxes can be opened
+        False: not all boxes can be opened
+    '''
+    length = len(boxes)
+    keys = set()
+    opened_boxes = []
+    i = 0
 
-    while keys:
-        current_key = keys.pop()
-        for key in boxes[current_key]:
-            if key < n and not opened[key]:
-                opened[key] = True
-                keys.append(key)
+    while i < length:
+        oldi = i
+        opened_boxes.append(i)
+        keys.update(boxes[i])
+        for key in keys:
+            if key != 0 and key < length and key not in opened_boxes:
+                i = key
+                break
+        if oldi != i:
+            continue
+        else:
+            break
 
-    return all(opened)
-
+    for i in range(length):
+        if i not in opened_boxes and i != 0:
+            return False
+    return True
