09a39c136eee52093f84b2c2bbf2c5942bafe00e
Benny254 <asshimbenny01@gmail.com>
2024-10-09 20:55:43 +0000

    update

--- 0-lockboxes.py/0-lockboxes.py
+++ 0-lockboxes.py/0-lockboxes.py
@@ -1,32 +1,13 @@
-#!/usr/bin/python3
-'''LockBoxes Challenge'''
-
-
 def canUnlockAll(boxes):
-    '''determines if all the boxes can be opened or not
-    Returns:
-        True: all boxes can be opened
-        False: not all boxes can be opened
-    '''
-    length = len(boxes)
-    keys = set()
-    opened_boxes = []
-    i = 0
+    opened = set([0])  # Start with the first box opened
+    keys = boxes[0]    # Get the keys from the first box
+    queue = keys       # Queue to manage boxes to explore
+    
+    while queue:
+        key = queue.pop()
+        if key not in opened and key < len(boxes):
+            opened.add(key)
+            queue.extend(boxes[key])
+    
+    return len(opened) == len(boxes)
 
-    while i < length:
-        oldi = i
-        opened_boxes.append(i)
-        keys.update(boxes[i])
-        for key in keys:
-            if key != 0 and key < length and key not in opened_boxes:
-                i = key
-                break
-        if oldi != i:
-            continue
-        else:
-            break
-
-    for i in range(length):
-        if i not in opened_boxes and i != 0:
-            return False
-    return True
