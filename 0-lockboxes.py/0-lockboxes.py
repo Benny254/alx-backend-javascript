47c357870ac93936766f38f3c4b411e57327ec2c
Benny254 <asshimbenny01@gmail.com>
2024-10-09 20:58:37 +0000

    update

--- 0-lockboxes.py/0-lockboxes.py
+++ 0-lockboxes.py/0-lockboxes.py
@@ -1,13 +1,35 @@
-def canUnlockAll(boxes):
-    opened = set([0])  # Start with the first box opened
-    keys = boxes[0]    # Get the keys from the first box
-    queue = keys       # Queue to manage boxes to explore
-    
-    while queue:
-        key = queue.pop()
-        if key not in opened and key < len(boxes):
-            opened.add(key)
-            queue.extend(boxes[key])
-    
-    return len(opened) == len(boxes)
+#!/usr/bin/python3
+'''
+Module Docs
+'''
 
+
+def canUnlockAll(boxes):
+    '''
+    Function Docs
+    '''
+    if (boxes):
+        numOfBoxes = len(boxes)
+        setOfKeys = {0}
+        setOfKeys.update(boxes[0])
+        visitedBoxes = {0}
+        while True:
+            newBoxVisited = False
+            keys = setOfKeys.copy()
+            # print(f"Set of keys copy = {keys}")
+            for key in keys:
+                # print(f"Key in loop = {key}")
+                if key not in visitedBoxes.copy() and key < numOfBoxes:
+                    # print(f"box opened = {key}")
+                    setOfKeys.update(boxes[key])
+                    visitedBoxes.add(key)
+                    # print(f"set of Keys updated = {setOfKeys}")
+                    # print(f"visitedBoxes updated = {visitedBoxes}")
+                    newBoxVisited = True
+            if not newBoxVisited:
+                break
+        for n in range(numOfBoxes):
+            if n not in setOfKeys:
+                return False
+        return True
+    return False
