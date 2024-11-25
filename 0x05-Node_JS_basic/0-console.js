2403819976a46ce49d45350b8b04f561713dc4ef
Benny254 <asshimbenny01@gmail.com>
2024-11-26 00:12:37 +0300

    update

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,10 @@
-function displayMessage(message) {
-     console.log(message);
-   }
+/**
+  * Function: displayMessage
+   * Description: Prints the provided string argument to STDOUT.
+    * @param {string} message - The message to be displayed.
+     */
+     function displayMessage(message) {
+         console.log(message);
+         }
 
-     displayMessage("Hello, World!") ;
+         displayMessage("Hello, World!");         
