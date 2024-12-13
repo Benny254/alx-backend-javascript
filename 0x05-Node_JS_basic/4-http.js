9aff514fc07773b9f97f412a5c77046d12fdcd75
Benny254 <asshimbenny01@gmail.com>
2024-12-13 19:01:18 +0300

    update

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,16 +1,18 @@
-const http = require('http');
+const http = require('http')
 
-const hostname = 'localhost';
-const port = 1245;
+const hostname = 'localhost'
+const port = 1245
 
-const app = http.createServer((req, res) => {
-	  res.statusCode = 200;
-	  res.setHeader('Content-Type', 'text/plain');
-	  res.end('Hello Holberton School!');
-});
+const app = http.createServer((req, res)=> {
+    res.statusCode=200
 
-app.listen(port, hostname, () => {
-	  console.log(`Server running at http://${hostname}:${port}/`);
-});
+	res.setHeader('Content-Type', 'text/plain')
+	res.end('Hello Alx School!')
+})
 
-module.exports = app;
+app.listen(port, hostname, ()=>{
+	    console.log('Server running at http://' + hostname + ':' + port + '/')
+})
+
+module.exports = app
+
